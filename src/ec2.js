5075bad4d8434d1cbfd0ff4253980ac08e0759f6
Roee Landesman <roee@robustintelligence.com>
2024-02-23 13:38:49 -0800

    Explicitly require IMDSv2 connection on EC2 creation

--- src/ec2.js
+++ src/ec2.js
@@ -348,7 +348,11 @@ const createEC2Instance = async function ({
     }],
     IamInstanceProfile: {
       Arn: app.state.stack.outputs.instanceProfileArn
-    }
+    },
+    MetadataOptions: { // Specify IMDSv2
+      HttpTokens: "required",
+      HttpPutResponseHopLimit: 3,
+    },
   };
   // specifiy instance profile:
   // https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/iam-roles-for-amazon-ec2.html#instance-profiles
