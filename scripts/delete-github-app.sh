dd1c396c30a883f31669831ff6b07186ffe06fb1
Cyril Rohr <hey@cyrilrohr.com>
2025-05-03 21:59:22 +0200

    More testing automation

---
+++ scripts/delete-github-app.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail
+
+safe_apps=("runs-on-dev" "runs-on-runs-on-stage")
+
+apps=$(gh api \
+  -H "Accept: application/vnd.github+json" \
+  /orgs/runs-on/installations \
+  --jq '.installations[].app_slug')
+
+for app in $apps; do
+  if [[ ! "$app" =~ ^RunsOn ]] || [[ " ${safe_apps[@]} " =~ " ${app} " ]]; then
+    echo "Skipping $app"
+    continue
+  fi
+  read -p "Remove $app? [y/N] " response
+  if [[ "$response" =~ ^[Yy]$ ]]; then
+    echo "${app//-runs-on/ [runs-on]}" | pbcopy
+    open "https://github.com/organizations/runs-on/settings/apps/$app/advanced"
+  fi
+done
