88754b2dac5c99bc1777d32ba81b9fbbcc37d86b
Cyril Rohr <hey@cyrilrohr.com>
2025-05-03 16:28:49 +0200

    Bump templates

--- scripts/set-bootstrap-tag.sh
+++ scripts/set-bootstrap-tag.sh
@@ -6,4 +6,4 @@ BOOTSTRAP_TAG=$(gh release list --repo runs-on/bootstrap --limit 1 | cut -f1)
 echo "Latest bootstrap tag: $BOOTSTRAP_TAG"
 
 # Update template-dev.yaml with latest bootstrap tag
-sed -i.bak "s|BootstrapTag:.*|BootstrapTag: $BOOTSTRAP_TAG|" cloudformation/template-dev.yaml
+sed -i.bak "s|BootstrapTag: v.*|BootstrapTag: $BOOTSTRAP_TAG|" cloudformation/template-dev.yaml
