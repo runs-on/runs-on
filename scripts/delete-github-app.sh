0b8bec242d6aaabfa1bf1fb3f9fa09786b25492b
Cyril Rohr <hey@cyrilrohr.com>
2025-05-27 22:49:08 +0200

    Add scripts/README.md

--- scripts/delete-github-app.sh
+++ scripts/delete-github-app.sh
@@ -15,7 +15,12 @@ for app in $apps; do
   fi
   read -p "Remove $app? [y/N] " response
   if [[ "$response" =~ ^[Yy]$ ]]; then
-    echo "${app//-runs-on/ [runs-on]}" | pbcopy
+    # for convenience
+    if [[ "$app" == "runson-test-runs-on" ]]; then
+      echo "RunsOn test [runs-on]" | pbcopy
+    else
+      echo "${app//-runs-on/ [runs-on]}" | pbcopy
+    fi
     open "https://github.com/organizations/runs-on/settings/apps/$app/advanced"
   fi
 done
