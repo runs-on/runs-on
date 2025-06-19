e322beba858faf5da8fe6ecf215dbdb3b21cee37
Cyril Rohr <hey@cyrilrohr.com>
2025-06-19 20:59:45 +0200

    Auto-update latest actions/runner version

--- scripts/set-bootstrap-tag.sh
+++ scripts/set-bootstrap-tag.sh
@@ -7,3 +7,9 @@ echo "Latest bootstrap tag: $BOOTSTRAP_TAG"
 
 # Update template-dev.yaml with latest bootstrap tag
 sed -i.bak "s|BootstrapTag: v.*|BootstrapTag: $BOOTSTRAP_TAG|" cloudformation/template-dev.yaml
+
+# Get latest release tag from runner repo
+RUNNER_TAG=$(gh release list --repo actions/runner --limit 1 | cut -f1)
+echo "Latest runner tag: $RUNNER_TAG"
+
+sed -i.bak "s|RUNS_ON_AGENT_VERSION = .*|RUNS_ON_AGENT_VERSION = \"${RUNNER_TAG#v}\"|" server/pkg/agent/constants.go
